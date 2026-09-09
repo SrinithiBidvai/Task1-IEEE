fcfaadecfa132e9275b25488e725063b593591f7
SrinithiBidvai <srinithi.bidvai@gmail.com>
2026-09-09 11:40:29 +0530

    Add files via upload

---
+++ LC424.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+
+        int left = 0;
+        int maxCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'A';
+
+            freq[index]++;
+            maxCount = Math.max(maxCount, freq[index]);
+
+            while ((right - left + 1) - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
