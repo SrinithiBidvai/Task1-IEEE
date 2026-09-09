430e7622fb71ecb0575b194f07ffcb145dc4c1ab
SrinithiBidvai <srinithi.bidvai@gmail.com>
2026-09-09 11:48:57 +0530

    Add files via upload

---
+++ LC876.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int len=0;
+        ListNode temp=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        ListNode a=head;
+        len=len/2;
+        while(len>0){
+            len--;
+            a=a.next;
+        }
+        return a;
+    }
+}
