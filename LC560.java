56592fe30e252d648fa4e01ab767e8c6cf9f825a
SrinithiBidvai <srinithi.bidvai@gmail.com>
2026-09-09 11:44:07 +0530

    Add files via upload

---
+++ LC560.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int ans=0;
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            s=0;
+            for(int j=i;j<nums.length;j++){
+                s+=nums[j];
+                if(s==k){
+                    ans+=1;
+                }
+            }
+        }
+        return ans;
+    }
+}
